52d3770f470f35758d9031441be38ffa27b31326
Mohit Soni <mohit19699soni@gmail.com>
2020-10-21 13:53:09 +0530

    server.js change

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
   })
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
   // Handle React routing, return all requests to React app
   app.use('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.js'));
